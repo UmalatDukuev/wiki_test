f834916f7e0dc70003278eb99a28699ce69e318b
Umalat <dukuev037@mail.ru>
2024-03-31 22:10:52 +0300

    первый пункт

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Unit test for simple App.
@@ -19,11 +20,20 @@ public class AppTest
     @Test
     public void testWikipediaPage() {
         //здесь нужно поменять путь к своему файлу msedgedriver.exe
+        
         System.setProperty("webdriver.edge.driver", "C:\\Users\\dukue\\Downloads\\edgedriver_win64_123\\msedgedriver.exe");
+        //System.setProperty("webdriver.gecko.driver", "C:\\Users\\dukue\\Downloads\\geckodriver-v0.34.0-win-aarch64\\geckodriver.exe");
 
         WebDriver driver = new EdgeDriver();
-
-
+        //WebDriver driver = new FirefoxDriver();
+        
+        
+        
+        
+        
+        
+        
+        driver.manage().window().maximize();
         String wikipediaURL = "https://ru.wikipedia.org/wiki/Заглавная_страница";
 
         driver.get(wikipediaURL);
@@ -39,11 +49,21 @@ public class AppTest
         List<WebElement> elements = driver.findElements(By.className("suggestions-result"));
         for (int i = 0; i < 5; i++){
             WebElement Suggest = elements.get(i);
+            String boldText = Suggest.findElement(By.className("highlight")).getText();
+
+            String regularText = Suggest.getText().replace(boldText, "");
             assertTrue(Suggest.getText().contains(inputString));
             System.out.println(Suggest.getText());
+            System.out.println(boldText);
+            System.out.println(regularText);
+
+
+            // WebElement boldTextElement = suggestion.findElement(By.className("highlight"));
+            // String boldText = boldTextElement.getText();
+            // String regularText = suggestion.getText().replace(boldText, "");
         }
         try {
-            Thread.sleep(3000); 
+            Thread.sleep(1000); 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
