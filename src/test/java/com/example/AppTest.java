c9b1a3b6ca8ac2a25a1ea2370e6c9c4ee6821752
Umalat <dukuev037@mail.ru>
2024-03-31 18:53:01 +0300

    проверка поисковых результатов

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,19 +2,53 @@ package com.example;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testWikipediaPage() {
+        //здесь нужно поменять путь к своему файлу msedgedriver.exe
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\dukue\\Downloads\\edgedriver_win64_123\\msedgedriver.exe");
+
+        WebDriver driver = new EdgeDriver();
+
+
+        String wikipediaURL = "https://ru.wikipedia.org/wiki/Заглавная_страница";
+
+        driver.get(wikipediaURL);
+        String inputString = "Тес";
+        String pageTitle = driver.getTitle();
+        assertTrue(pageTitle.contains("Википедия"));
+        driver.findElement(By.className("vector-search-box-input")).sendKeys(inputString);
+        try {
+            Thread.sleep(500); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<WebElement> elements = driver.findElements(By.className("suggestions-result"));
+        for (int i = 0; i < 5; i++){
+            WebElement Suggest = elements.get(i);
+            assertTrue(Suggest.getText().contains(inputString));
+            System.out.println(Suggest.getText());
+        }
+        assertTrue(1 == 1);
+        try {
+            Thread.sleep(3000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Закрытие браузера
+        driver.quit();
     }
 }
